6e1d9212f2f508eaaa97532a2687561ef46ef3e3
jonathafernandes <jonathafernandes@live.com>
2024-02-21 11:52:08 -0300

    refactor: remove favorite teams

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -7,37 +7,30 @@ const Main = () => {
     const [team, setTeam] = useState([
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time A",
         },
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time B",
         },
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time C",
         },
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time D",
         },
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time E",
         },
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time F",
         },
         {
             id: uuidv4(),
-            favorite: false,
             name: "Time G",
         },
     ]);
