5d04c64085e33b693ce3ff0b2a21331189da0e25
jonathafernandes <jonathafernandes@live.com>
2024-02-22 17:10:23 -0300

    feat: added smooth scroll

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
     return (
         <footer id="footer" className="mb-10 flex flex-col items-center gap-8">
             <RiInformationFill className="text-blue-500" size={30} />
-            <p className="text-center mx-4 sm:mx-auto sm:w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ut facere officiis obcaecati est ipsum quibusdam eligendi reprehenderit impedit necessitatibus iusto eius odio repellat quidem totam culpa, ad ullam unde.</p>
+            <p className="text-center text-lg mx-4 sm:mx-auto sm:w-1/2">
+                Crie colaboradores, escolha entre os times padrões ou crie o seu próprio time, defina a cor de cada time.
+            </p>
             <p className="text-center text-zinc-400">&copy; 2024 Organo.</p>
         </footer>
     );
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,13 +1,21 @@
 import logo from '../assets/logoipsum-296.svg';
 
 const Header = () => {
+
+    const handleSmoothScroll = (event) => {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute('href').slice(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <header className='flex items-center gap-10 flex-col sm:flex-row sm:justify-around m-5'>
             <div className='flex items-center gap-2'>
                 <img src={logo} alt="Organo" />
                 <span className='text-xl font-bold'>Organo</span>
             </div>
-            <a href='#footer' className='border-solid border-2 border-blue-500 bg-blue-600 rounded p-2 font-medium hover:bg-blue-500 cursor-pointer'>
+            <a href='#footer' onClick={handleSmoothScroll} className='border-solid border-2 border-blue-500 bg-blue-600 rounded p-2 font-medium hover:bg-blue-500 cursor-pointer'>
                 Saiba mais
             </a>
         </header>
